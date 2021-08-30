a0ccdef96b00dfc79aa425f1c17dbc52d6775039
MaximSamusenko <all-by-my-self@yandex.ru>
2021-08-30 12:16:59 +0300

    update index.js

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-import { CollapsePane, CollapsePaneProps } from "./src/components/CollapsePane";
-
-export default CollapsePane;
-
-export { CollapsePaneProps };
--- src/index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./redux-react-collapse-pane.cjs.production.min.js')
-} else {
-    module.exports = require('./redux-react-collapse-pane.cjs.development.js')
-}
